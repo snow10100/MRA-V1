0ffe41136a0b684d1c885fb305b44bedbec79857
snow10100 <169849961+snow10100@users.noreply.github.com>
2024-05-15 10:42:02 +0300

    built ros2 package for arm_desc

---
+++ arm_desc/launch/display.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    pkg_path = FindPackageShare('arm_desc')
+    default_model_path = PathJoinSubstitution(['urdf', 'robot.urdf'])
+    default_rviz_config_path = PathJoinSubstitution([pkg_path, 'rviz', 'urdf.rviz'])
+
+    # These parameters are maintained for backwards compatibility
+    gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
+                                    description='Flag to enable joint_state_publisher_gui')
+    ld.add_action(gui_arg)
+    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
+                                     description='Absolute path to rviz config file')
+    ld.add_action(rviz_arg)
+
+    # This parameter has changed its meaning slightly from previous versions
+    ld.add_action(DeclareLaunchArgument(name='model', default_value=default_model_path,
+                                        description='Path to robot urdf file relative to arm_desc package'))
+
+    ld.add_action(IncludeLaunchDescription(
+        PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
+        launch_arguments={
+            'urdf_package': 'arm_desc',
+            'urdf_package_path': LaunchConfiguration('model'),
+            'rviz_config': LaunchConfiguration('rvizconfig'),
+            'jsp_gui': LaunchConfiguration('gui')}.items()
+    ))
+
+    return ld
